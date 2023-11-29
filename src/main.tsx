0ed95083456c7611a0affbfc01b95bff68dd92ff
Pusaka Manggala <pusakamanggala@gmail.com>
2023-11-29 17:39:28 +0700

    Add initial project files

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={new QueryClient()}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          main: "#13293D",
+          1: "#006493",
+          2: "#247B9F",
+          3: "#1B97DF",
+          4: "#EAF4F4",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
