cff1217f9f7f40dd5c8d8539f40572a40d10fe7d
Pusaka Manggala <pusakamanggala@gmail.com>
2023-11-30 17:08:57 +0700

    Add Manrope font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
+      },
       colors: {
         blue: {
           main: "#13293D",
