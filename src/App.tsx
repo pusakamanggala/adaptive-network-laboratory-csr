89547fe1b70de8217c41b758608d19ae8c526f14
Pusaka Manggala <pusakamanggala@gmail.com>
2023-12-02 16:22:11 +0700

    add route with react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { HomePage } from "./pages/HomePage";
 import { PageLayout } from "./layout/PageLayout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <PageLayout>
-        <HomePage />
-      </PageLayout>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PageLayout>
+              <HomePage />
+            </PageLayout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
