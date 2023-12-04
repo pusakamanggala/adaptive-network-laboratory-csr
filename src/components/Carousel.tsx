70896b92b990a504ac5b93a5f8861d1fb3dd80ec
Pusaka Manggala <pusakamanggala@gmail.com>
2023-12-04 15:23:23 +0700

    refractor : carousel component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,32 +1,42 @@
-import { useRef, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
-type CarouselProps = {
+interface CarouselProps {
   children: React.ReactNode;
-};
+}
 
 const Carousel = ({ children }: CarouselProps) => {
   const slidesContainerRef = useRef<HTMLDivElement>(null);
   const [slideWidth, setSlideWidth] = useState<number | undefined>(undefined);
-
-  const handleNext = () => {
-    if (slidesContainerRef.current && slideWidth !== undefined) {
-      slidesContainerRef.current.scrollLeft += slideWidth;
-    }
-  };
-
-  const handlePrev = () => {
-    if (slidesContainerRef.current && slideWidth !== undefined) {
-      slidesContainerRef.current.scrollLeft -= slideWidth;
-    }
-  };
+  const maxScrollLeft = useRef<number>(0);
 
   useEffect(() => {
-    const updatedSlideWidth =
-      slidesContainerRef.current?.querySelector(".slide")?.clientWidth;
-    if (updatedSlideWidth !== undefined) {
-      setSlideWidth(updatedSlideWidth);
+    if (slidesContainerRef.current) {
+      setSlideWidth(slidesContainerRef.current.offsetWidth);
+      maxScrollLeft.current =
+        slidesContainerRef.current.scrollWidth -
+        slidesContainerRef.current.clientWidth;
     }
   }, []);
+
+  const handleNext = useCallback(() => {
+    if (
+      slidesContainerRef.current &&
+      slideWidth !== undefined &&
+      slidesContainerRef.current.scrollLeft < maxScrollLeft.current
+    ) {
+      slidesContainerRef.current.scrollLeft += slideWidth!;
+    }
+  }, [slideWidth]);
+
+  const handlePrev = useCallback(() => {
+    if (
+      slidesContainerRef.current &&
+      slideWidth !== undefined &&
+      slidesContainerRef.current.scrollLeft > 0
+    ) {
+      slidesContainerRef.current.scrollLeft -= slideWidth!;
+    }
+  }, [slideWidth]);
 
   return (
     <div className="relative">
@@ -34,31 +44,6 @@ const Carousel = ({ children }: CarouselProps) => {
         ref={slidesContainerRef}
         className="slides-container h-72 flex snap-x snap-mandatory overflow-hidden overflow-x-auto space-x-5 rounded scroll-smooth before:w-[45vw] before:shrink-0 after:w-[45vw] after:shrink-0 md:before:w-0 md:after:w-0"
       >
-        {/* {carouselData &&
-          carouselData.map((carousel) => (
-            <div
-              className={`flex-shrink-0 h-full overflow-hidden rounded slide snap-center ${
-                carousel.desc && carousel.title && "w-[900px]"
-              } `}
-            >
-              <div className="flex h-full">
-                {carousel.desc && carousel.title && (
-                  <article className="space-y-4">
-                    <h1 className="w-1/3 text-2xl font-bold text-blue-3">
-                      {carousel.title}
-                    </h1>
-                    <p>{carousel.desc}</p>
-                  </article>
-                )}
-                <img
-                  className={`object-cover h-full rounded-xl ${
-                    carousel.desc && carousel.title && "w-2/3"
-                  }`}
-                  src={carousel.imageUrl}
-                />
-              </div>
-            </div>
-          ))} */}
         {children}
       </div>
       <div className="absolute top-0 items-center hidden h-full -left-4 md:flex">
